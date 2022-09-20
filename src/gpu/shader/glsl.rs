6e67d4d670ad07a87a65dc61012be1aed4bc43f0
Desmond Germans <desmond@germansmedia.nl>
2022-09-20 08:54:41 +0200

    starting GLSL renderer

--- src/gpu/shader/glsl.rs
+++ src/gpu/shader/glsl.rs
@@ -2,9 +2,9 @@ use {
     crate::*,
 };
 
-/*
-fn gl_base_type_name(ty: &sr::BaseType) -> &'static str {
-    match ty {
+fn gl_base_type_name(type_: &sr::BaseType) -> &'static str {
+    match type_ {
+        sr::BaseType::Bool => "bool",
         sr::BaseType::U8 => "uint",
         sr::BaseType::U16 => "uint",
         sr::BaseType::U32 => "uint",
@@ -16,6 +16,7 @@ fn gl_base_type_name(ty: &sr::BaseType) -> &'static str {
         sr::BaseType::F16 => "float",
         sr::BaseType::F32 => "float",
         sr::BaseType::F64 => "double",
+        sr::BaseType::Vec2Bool => "bvec2",
         sr::BaseType::Vec2U8 => "uvec2",
         sr::BaseType::Vec2U16 => "uvec2",
         sr::BaseType::Vec2U32 => "uvec2",
@@ -27,6 +28,7 @@ fn gl_base_type_name(ty: &sr::BaseType) -> &'static str {
         sr::BaseType::Vec2F16 => "vec2",
         sr::BaseType::Vec2F32 => "vec2",
         sr::BaseType::Vec2F64 => "vec2",
+        sr::BaseType::Vec3Bool => "bvec3",
         sr::BaseType::Vec3U8 => "uvec3",
         sr::BaseType::Vec3U16 => "uvec3",
         sr::BaseType::Vec3U32 => "uvec3",
@@ -38,6 +40,7 @@ fn gl_base_type_name(ty: &sr::BaseType) -> &'static str {
         sr::BaseType::Vec3F16 => "vec3",
         sr::BaseType::Vec3F32 => "vec3",
         sr::BaseType::Vec3F64 => "vec3",
+        sr::BaseType::Vec4Bool => "bvec4",
         sr::BaseType::Vec4U8 => "uvec4",
         sr::BaseType::Vec4U16 => "uvec4",
         sr::BaseType::Vec4U32 => "uvec4",
@@ -57,69 +60,221 @@ fn gl_base_type_name(ty: &sr::BaseType) -> &'static str {
     }
 }
 
-fn collect_layouts(symbol: &str,ty: &sr::Type,vertex_symbol: &str,vertex_fields: &Vec<(String,sr::BaseType)>) -> Vec<(String,String)> {
-    match ty {
-        sr::Type::Array(_,_) => panic!("unable to handle array parameters just yet"),
-        sr::Type::Base(base_type) => vec![(gl_base_type_name(&base_type).to_string(),symbol.to_string())],
-        sr::Type::Inferred => panic!("unable to handle inferred parameters just yet"),
-        sr::Type::Symbol(type_symbol) => if type_symbol == vertex_symbol {
-            let mut layouts: Vec<(String,String)> = Vec::new();
-            for (symbol,ty) in vertex_fields {
-                layouts.push((gl_base_type_name(ty).to_string(),symbol.to_string()));
-            }
-            layouts
-        }
-        else {
-            panic!("unable to handle symbol parameter other than the vertex just yet");
-        },
-        sr::Type::Tuple(types) => {
-            let mut layouts: Vec<(String,String)> = Vec::new();
-            let mut i = 0usize;
-            for ty in types {
-                layouts.extend_from_slice(&collect_layouts(&format!("{}{}",symbol,i),ty,vertex_symbol,vertex_fields));
-                i += 1;
-            }
-            layouts
-        },
+fn render_type(type_: &sr::Type) -> String {
+    
+}
+
+fn render_stat(stat: &sr::Stat) -> String {
+    match stat {
+        sr::Stat::Let(ident,type_,expr) => if let Some(expr) = expr { format!("{} {} = {};",render_type(type_),ident,render_expr(expr)) } else { format!("{} {};",render_type(type_),ident) },
+        sr::Stat::Expr(expr) => format!("{};",render_expr(expr)),
     }
 }
 
-pub fn compile_vertex_shader(items: Vec<sr::Item>,vertex_symbol: String,vertex_fields: Vec<(String,sr::BaseType)>) -> Option<Vec<u8>> {
-    println!("VERTEX SHADER:\ninput rust functions:\n");
-    for item in &items {
-        println!("{}",item);
+fn render_vec_literal(type_ident: &str,fields: &Vec<(String,sr::Expr)>) -> String {
+    let mut r = format!("{}(",type_ident);
+    let mut first_field = true;
+    for field in fields.iter() {
+        if !first_field {
+            r += ",";
+        }
+        r += &render_expr(&field.1);
+        first_field = false;
     }
+    r += ")";
+    r
+}
+
+fn render_expr(expr: &sr::Expr) -> String {
+    match expr {
+        sr::Expr::Boolean(value) => if *value { "true".to_string() } else { "false".to_string() },
+        sr::Expr::Integer(value) => format!("{}",value),
+        sr::Expr::Float(value) => format!("{}",value),
+        sr::Expr::Base(base_type,fields) => {
+            match base_type {
+                sr::BaseType::Vec2Bool => render_vec_literal("bvec2",fields),
+                sr::BaseType::Vec2U8 |
+                sr::BaseType::Vec2U16 |
+                sr::BaseType::Vec2U32 |
+                sr::BaseType::Vec2U64 => render_vec_literal("uvec2",fields),
+                sr::BaseType::Vec2I8 |
+                sr::BaseType::Vec2I16 |
+                sr::BaseType::Vec2I32 |
+                sr::BaseType::Vec2I64 => render_vec_literal("ivec2",fields),
+                sr::BaseType::Vec2F16 |
+                sr::BaseType::Vec2F32 => render_vec_literal("vec2",fields),
+                sr::BaseType::Vec2F64 => render_vec_literal("dvec2",fields),
+                sr::BaseType::Vec3Bool => render_vec_literal("bvec3",fields),
+                sr::BaseType::Vec3U8 |
+                sr::BaseType::Vec3U16 |
+                sr::BaseType::Vec3U32 |
+                sr::BaseType::Vec3U64 => render_vec_literal("uvec3",fields),
+                sr::BaseType::Vec3I8 |
+                sr::BaseType::Vec3I16 |
+                sr::BaseType::Vec3I32 |
+                sr::BaseType::Vec3I64 => render_vec_literal("ivec3",fields),
+                sr::BaseType::Vec3F16 |
+                sr::BaseType::Vec3F32 => render_vec_literal("vec3",fields),
+                sr::BaseType::Vec3F64 => render_vec_literal("dvec3",fields),
+                sr::BaseType::Vec4Bool => render_vec_literal("bvec4",fields),
+                sr::BaseType::Vec4U8 |
+                sr::BaseType::Vec4U16 |
+                sr::BaseType::Vec4U32 |
+                sr::BaseType::Vec4U64 => render_vec_literal("uvec4",fields),
+                sr::BaseType::Vec4I8 |
+                sr::BaseType::Vec4I16 |
+                sr::BaseType::Vec4I32 |
+                sr::BaseType::Vec4I64 => render_vec_literal("ivec4",fields),
+                sr::BaseType::Vec4F16 |
+                sr::BaseType::Vec4F32 => render_vec_literal("vec4",fields),
+                sr::BaseType::Vec4F64 => render_vec_literal("dvec4",fields),
+                sr::BaseType::ColorU8 |
+                sr::BaseType::ColorU16 |
+                sr::BaseType::ColorF16 |
+                sr::BaseType::ColorF32 => render_vec_literal("vec4",fields),
+                sr::BaseType::ColorF64 => render_vec_literal("dvec4",fields),
+                _ => panic!("ERROR: small basetype literals don't show up directly"),
+            }
+        },
+        sr::Expr::Local(variable) |
+        sr::Expr::Param(variable) |
+        sr::Expr::Const(variable) => variable.ident.clone(),
+        sr::Expr::Array(exprs) => {
+            let mut r = format!("TODO[{}](",exprs.len());
+            let mut first_expr = false;
+            for expr in exprs.iter() {
+                if !first_expr {
+                    r += ",";
+                }
+                r += &render_expr(expr);
+                first_expr = false;
+            }
+            r += ")";
+            r
+        },
+        sr::Expr::Cloned(expr,expr2) => format!("TODO temp = {}; TODO[{}](TODO)",expr,expr2),
+        sr::Expr::Struct(struct_,fields) => {
+            let mut r = format!("{}(",struct_.ident);
+            let mut first_field = false;
+            for field in fields.iter() {
+                if !first_field {
+                    r += ",";
+                }
+                r += &render_expr(&field.1);
+                first_field = false;
+            }
+            r += ")";
+            r
+        },
+        sr::Expr::Call(function,exprs) => {
+            let mut r = format!("{}(",function.ident);
+            let mut first_expr = false;
+            for expr in exprs.iter() {
+                if !first_expr {
+                    r += ",";
+                }
+                r += &render_expr(expr);
+                first_expr = false;
+            }
+            r += ")";
+            r
+        },
+        sr::Expr::Field(expr,ident) => format!("{}.{}",render_expr(expr),ident),
+        sr::Expr::Index(expr,expr2) => format!("({}[{}])",render_expr(expr),render_expr(expr2)),
+        sr::Expr::Cast(expr,type_) => format!("(({}){})",render_type(type_),render_expr(expr)),
+        sr::Expr::Neg(expr) => format!("(-{})",render_expr(expr)),
+        sr::Expr::Not(expr) => format!("(!{})",render_expr(expr)),
+        sr::Expr::Mul(expr,expr2) => format!("({}*{})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::Div(expr,expr2) => format!("({}/{})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::Mod(expr,expr2) => format!("({}%{})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::Add(expr,expr2) => format!("({}+{})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::Sub(expr,expr2) => format!("({}-{})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::Shl(expr,expr2) => format!("({}<<{})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::Shr(expr,expr2) => format!("({}>>{})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::And(expr,expr2) => format!("({}&{})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::Or(expr,expr2) => format!("({}|{})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::Xor(expr,expr2) => format!("({}^{})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::Eq(expr,expr2) => format!("({}=={})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::NotEq(expr,expr2) => format!("({}!={})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::Greater(expr,expr2) => format!("({}>{})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::Less(expr,expr2) => format!("({}<{})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::GreaterEq(expr,expr2) => format!("({}>={})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::LessEq(expr,expr2) => format!("({}<={})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::LogAnd(expr,expr2) => format!("({}&&{})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::LogOr(expr,expr2) => format!("({}||{})",render_expr(expr),render_expr(expr2)),
+        sr::Expr::Assign(expr,expr2) => format!("{}={}",render_expr(expr),render_expr(expr2)),
+        sr::Expr::AddAssign(expr,expr2) => format!("{}+={}",render_expr(expr),render_expr(expr2)),
+        sr::Expr::SubAssign(expr,expr2) => format!("{}-={}",render_expr(expr),render_expr(expr2)),
+        sr::Expr::MulAssign(expr,expr2) => format!("{}*={}",render_expr(expr),render_expr(expr2)),
+        sr::Expr::DivAssign(expr,expr2) => format!("{}/={}",render_expr(expr),render_expr(expr2)),
+        sr::Expr::ModAssign(expr,expr2) => format!("{}%={}",render_expr(expr),render_expr(expr2)),
+        sr::Expr::AndAssign(expr,expr2) => format!("{}&={}",render_expr(expr),render_expr(expr2)),
+        sr::Expr::OrAssign(expr,expr2) => format!("{}|={}",render_expr(expr),render_expr(expr2)),
+        sr::Expr::XorAssign(expr,expr2) => format!("{}^={}",render_expr(expr),render_expr(expr2)),
+        sr::Expr::ShlAssign(expr,expr2) => format!("{}<<={}",render_expr(expr),render_expr(expr2)),
+        sr::Expr::ShrAssign(expr,expr2) => format!("{}>>={}",render_expr(expr),render_expr(expr2)),
+        sr::Expr::Continue => "continue".to_string(),
+        sr::Expr::Break(_) => "break".to_string(),
+        sr::Expr::Return(expr) => if let Some(expr) = expr { format!("return {}",render_expr(expr)) } else { "return".to_string() },
+        sr::Expr::Block(block) => format!("{{ {} }}",render_block(block)),
+        sr::Expr::If(expr,block,else_expr) => if let Some(else_expr) = else_expr {
+            format!("if ({}) {{ {} }} else {}",render_expr(expr),render_block(block),render_expr(else_expr))
+        }
+        else {
+            format!("if ({}) {{ {} }}",render_expr(expr),render_block(block))
+        },
+        sr::Expr::Loop(block) => format!("while (true) {{ {} }}",render_block(block)),
+        sr::Expr::For(ident,range,block) => format!("for (int {} = TODO; {} < TODO; {}++) {{ {} }}",ident,ident,ident,render_block(block)),
+        sr::Expr::While(expr,block) => format!("while ({}) {{ {} }}",render_expr(expr),render_block(block)),
+        _ => panic!("ERROR: no Unknown* nodes should exist here"),
+    }
+}
+
+fn render_block(block: &sr::Block) -> String {
+    let mut r = String::new();
+    for stat in block.stats.iter() {
+        r += &render_stat(stat);
+    }
+    if let Some(expr) = block.expr {
+        r += &format!("return {}",render_expr(&expr));
+    }
+    r
+}
+
+fn collect_layouts(ident: &str,type_: &sr::Type) -> Vec<(String,String)> {
+    let mut layouts: Vec<(String,String)> = Vec::new();
+    match type_ {
+        sr::Type::Struct(struct_) => {
+            for field in struct_.fields.iter() {
+                let base_type = if let sr::Type::Base(base_type) = field.type_ { base_type } else { panic!("only base types supported in function parameter struct fields"); };
+                layouts.push((gl_base_type_name(&base_type).to_string(),format!("{}_{}",ident,field.ident)));
+            }
+        },
+        _ => panic!("unable to handle non-struct parameters just yet"),
+    }
+    layouts
+}
+
+pub fn compile_vertex_shader(module: sr::Module,vertex_ident: String,vertex_fields: Vec<sr::Field>) -> Option<Vec<u8>> {
+
+    process_vertex_shader(module,vertex_ident,vertex_fields);
 
     // find main function
-    let mut main: Option<(&Vec<(sr::Pat,Box<sr::Type>)>,&Option<Box<sr::Type>>,&Vec<sr::Stat>)> = None;
-    for item in &items { 
-        if let sr::Item::Function(symbol,params,return_ty,stats) = item {
-            if symbol == "main" {
-                main = Some((params,return_ty,stats));
-                break;
-            }
-        }
-    }
-    let (main_params,main_return_ty,_main_stats) = main.expect("main function missing in vertex shader");
+    let main = &module.functions.get("main").expect("main function missing");
 
     // collect input layouts
     let mut input_layouts: Vec<(String,String)> = Vec::new();
-    for (pat,ty) in main_params {
-        if let sr::Pat::Symbol(symbol) = pat {
-            input_layouts.extend_from_slice(&collect_layouts(symbol,ty,&vertex_symbol,&vertex_fields));
-        }
-        else {
-            panic!("only symbol allowed as parameter pattern (not {})",pat);
-        }
+    for param in main.params {
+        input_layouts.extend_from_slice(&collect_layouts(&param.ident,&param.type_));
     }
-
+    
     // collect output layouts
-    let main_return_ty = if let Some(main_return_ty) = &main_return_ty { main_return_ty } else { panic!("main should have a return type") };
-    let output_layouts = collect_layouts("output",main_return_ty,&vertex_symbol,&vertex_fields);
-    if let output_layouts[0].1 != "vec4" {
-        panic!("output should be a vec4 for the position, optionally followed by other parameters");
+    let main_return_type = if let sr::Type::Void = main.return_type { panic!("main should return non-void"); } else { main.return_type };
+    let output_layouts = collect_layouts("output",&main_return_type);
+    if output_layouts[0].1 != "vec4" {
+        panic!("first element of the output should be a vec4 for the vertex position, optionally followed by other parameters");
     }
-
+    
     // render start
     let mut r = "#version 450\n".to_string();
 
@@ -134,57 +289,16 @@ pub fn compile_vertex_shader(items: Vec<sr::Item>,vertex_symbol: String,vertex_f
     }
 
     // render main code
-    r += "void main() {\n";
+    r += "void main() { ";
 
-    // TODO: the actual code
-    r += &format!("    gl_Position = vec4({}.x,{}.y,0.0,1.0);\n",vertex_fields[0].0,vertex_fields[0].0);
-    r += &format!("    todo = {};\n",vertex_fields[1].0);
+    r += &render_block(&main.block);
 
     r += "}\n";
 
-    println!("output:\n{}",r);
+    println!("GLSL code:\n{}",r);
 
     r += "\0";
     Some(r.into_bytes())
-}
-
-pub fn compile_fragment_shader(items: Vec<sr::Item>) -> Option<Vec<u8>> {
-    println!("FRAGMENT SHADER:\ninput:");
-    for item in items {
-        println!("{}",item);
-    }
-
-    // begin rendering
-    let mut r = "#version 450\n".to_string();
-
-    // TODO: input locations
-    r += "layout(location=0) in vec4 varying_;\n";
-
-    // TODO: output locations
-    r += "layout(location=0) out vec4 fragment;\n";
-
-    // main code
-    r += "void main() {\n";
-
-    // TODO: the actual code
-    r += "    fragment = varying_;\n";
-
-    r += "}\n";
-
-    println!("output:\n{}",r);
-
-    // TODO: convert r to raw C string in Vec<u8>
-
-    r += "\0";
-    Some(r.into_bytes())
-}
-*/
-
-pub fn compile_vertex_shader(module: sr::Module,vertex_ident: String,vertex_fields: Vec<sr::Field>) -> Option<Vec<u8>> {
-
-    process_vertex_shader(module,vertex_ident,vertex_fields);
-    println!("TODO: render GLSL");
-    None
 }
 
 pub fn compile_fragment_shader(module: sr::Module) -> Option<Vec<u8>> {
